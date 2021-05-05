cda295924d2f77a4802a0992ac60d9ed6fdccdc3
Michelle Rios <brendamichelle995@gmail.com>
2021-05-05 17:39:02 -0400

    udpate migrations

--- db/migrate/20210505203017_create_students.rb
+++ db/migrate/20210505203017_create_students.rb
@@ -1,8 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
-    t.string :name
-    t.string :degree
-    t.date :birthday
-    t.integer :year
+    create_table :students do |t|
+      t.string :name
+      t.string :degree
+      t.date :birthday
+      t.integer :year
+    end
   end
 end
--- db/migrate/20210505203024_create_exams.rb
+++ db/migrate/20210505203024_create_exams.rb
@@ -1,8 +1,10 @@
 class CreateExams < ActiveRecord::Migration[6.1]
   def change
-    t.string :subject
-    t.integer :total_questions
-    t.boolean :is_pop_quiz
-    t.date :administration_date
+    create_table :exams do |t|
+      t.string :subject
+      t.integer :total_questions
+      t.boolean :is_pop_quiz
+      t.date :administration_date
+    end
   end
 end
